fd572c0dc3f095148d90b1e8a0a84178dd3ce90d
Victoire Lefebvre <guylaine.a@wanadoo.fr>
2020-05-05 17:06:16 +0200

    ex3 partie1 php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$km = 'new number'; //déclaration variable
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8"/>
+  <title>ex1 partie1 php</title>
+</head>
+<body>
+  <?php
+  echo 'number'; //initialiser la valeur nombre
+  ?>
+</body>
+</html>
